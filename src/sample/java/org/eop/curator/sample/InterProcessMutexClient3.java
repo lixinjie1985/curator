50adfcc12ef36747fcee476d1981161a8811eee7
Admin <Admin@WIN-85JK86NCS29>
2017-03-25 16:37:45 +0800

    update

--- src/sample/java/org/eop/curator/sample/InterProcessMutexClient3.java
+++ src/sample/java/org/eop/curator/sample/InterProcessMutexClient3.java
@@ -13,6 +13,7 @@ public class InterProcessMutexClient3 {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("主线程：" + Thread.currentThread().getId());
+		final Thread mainThread = Thread.currentThread();
 		CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", new ExponentialBackoffRetry(1000, 3));
 		client.start();
 		InterProcessMutex mutex = new InterProcessMutex(client, "/mutex");
@@ -22,7 +23,17 @@ public class InterProcessMutexClient3 {
 			public void revocationRequested(InterProcessMutex forLock) {
 				System.out.println("事件线程：" + Thread.currentThread().getId());
 				try {
-					forLock.release();
+					//这里直接调用报错，需要持有锁的线程调用才行
+					//forLock.release();
+					//只要线程调用了mutex.acquire()方法，那么就参与到获取锁的队伍当中，就会有一个节点与之对应
+					//无论是获得了锁还是被阻塞等待获得锁，都可以被撤销，需要指定与之对应的节点来告知撤销哪个JVM里的锁
+					System.out.println("锁是否被当前JVM里某个线程持有：" + forLock.isAcquiredInThisProcess());
+					//检测锁是否被当前JVM里的线程持有，然后对应不同的撤销逻辑
+					if (forLock.isAcquiredInThisProcess()) {
+						mainThread.interrupt();
+					} else {
+						mainThread.interrupt();
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -32,9 +43,19 @@ public class InterProcessMutexClient3 {
 		mutex.acquire();
 		System.out.println("-------------------3已经获取一个锁------------------");
 		System.out.println("participant=" + mutex.getParticipantNodes());
-		Thread.sleep(20000);
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			System.out.println("提前释放锁线程：" + Thread.currentThread().getId());
+			System.out.println("-------------------3开始提前释放一个锁------------------");
+			mutex.release();
+			System.out.println("-------------------3已经提前释放一个锁------------------");
+			return;
+		}
 		System.out.println("-------------------3开始释放一个锁------------------");
 		mutex.release();
 		System.out.println("-------------------3已经释放一个锁------------------");
+		System.out.println("participant=" + mutex.getParticipantNodes());
+		Thread.sleep(20000);
 	}
 }
--- src/sample/java/org/eop/curator/sample/InterProcessMutexClient4.java
+++ src/sample/java/org/eop/curator/sample/InterProcessMutexClient4.java
@@ -2,6 +2,7 @@ package org.eop.curator.sample;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.framework.recipes.locks.Revoker;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
@@ -14,10 +15,11 @@ public class InterProcessMutexClient4 {
 		System.out.println("主线程：" + Thread.currentThread().getId());
 		CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", new ExponentialBackoffRetry(1000, 3));
 		client.start();
-		
+		InterProcessMutex mutex = new InterProcessMutex(client, "/mutex");
+		System.out.println("participant=" + mutex.getParticipantNodes());
 		System.out.println("-------------------4开始撤销一个锁------------------");
-		Revoker.attemptRevoke(client, "/mutex/_c_a72604d7-ab2d-4b81-ba48-1af5f354f580-lock-0000000000");
+		Revoker.attemptRevoke(client, "/mutex/_c_08c6029c-979a-4554-a659-26d534dc480b-lock-0000000002");
 		System.out.println("-------------------4已经撤销一个锁------------------");
-		Thread.sleep(15000);
+		Thread.sleep(1000);
 	}
 }
